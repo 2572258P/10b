ced47a3fd893051239cadcfcf559e1ea0138f5e4
Jungshic Park <2572258P@student.gla.ac.uk>
2021-03-04 15:49:50 +0000

    reversed all

--- team10b/settings.py
+++ team10b/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-12222
+
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -58,7 +58,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'team10b.urls123'
+ROOT_URLCONF = 'team10b.urls'
 
 TEMPLATES = [
     {
