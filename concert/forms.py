cd168849ce1bfd1e9fd715d9afa6b2868e06363a
Jungshic Park <2572258P@student.gla.ac.uk>
2021-03-18 22:20:29 +0000

    username -> email

--- concert/forms.py
+++ concert/forms.py
@@ -7,7 +7,7 @@ class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
     class Meta:
         model = User
-        fields = ('username','password','first_name','last_name','email')
+        fields = ('username','password','first_name','last_name')
         
         
        
